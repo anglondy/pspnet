de0a25989f2b6710fe250df3dee035dc2bb6bbb0
anglondy <91787295+anglondy@users.noreply.github.com>
2021-11-12 15:30:14 +0300

    Add files via upload

---
+++ constants/constants.py
@@ -0,0 +1,21 @@
+DATA_PATH = 'data'
+IMAGES_PATH = 'data/ade20k_sub/images/training'
+LABELS_PATH = 'data/ade20k_sub/annotations/training'
+INPUT_SHAPE = (300, 300, 3)
+NOT_RARE_CLASSES = [True,  True,  True,  True,  True,  True, False,  True, False,
+                    True,  True, False,  True,  True,  True,  True,  True,  True,
+                    True, False,  True,  True,  True, False, False, False,  True,
+                    True, False, False,  True, False, False,  True, False,  True,
+                    False, False, False,  True, False, False, False, False,  True,
+                    False, False,  True, False,  True, False, False, False,  True,
+                    True,  True, False, False, False, False,  True,  True,  True,
+                    False, False, False, False,  True, False,  True,  True, False,
+                    False,  True, False, False, False,  True, False, False,  True,
+                    True, False, False,  True,  True, False,  True,  True, False,
+                    False,  True,  True, False,  True,  True,  True, False, False,
+                    False, False, False, False,  True, False, False, False, False,
+                    False, False, False, False, False, False,  True, False, False,
+                    False, False, False, False, False, False, False, False, False,
+                    True, False,  True, False, False, False, False, False, False,
+                    False, False,  True, False,  True, False,  True, False, False,
+                    False, False, False, False, False, False,  True]
